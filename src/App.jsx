f6f5750a9a1f874a5f93409638a62e6fd6f8fda4
Demetria15 <73849534+Demetria15@users.noreply.github.com>
2023-10-22 22:20:12 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import data from "./data.json";
+import sidebarData from "./stretched-goal.json"
+import { Header } from "./components/Header";
+import Album from './components/Album';
+import Sidebar from './components/Sidebar';
+import Single from './components/Single';
+
 
 console.log(data);
 
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  return (
+    <div className="app">
+      <Header />
+      <div className="album-container">
+        <Album data={data} />
+        <Single data={data} />
+      </div>
+      <Sidebar sidebarData={sidebarData} />
+    </div>
+  );
 };
